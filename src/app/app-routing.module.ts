3937dc66f096d40eb0cff5e60b1f511be0120a37
GABRIELE Giunchi <gabriele.giunchi@studio.unibo.it>
2019-02-14 10:30:11 +0100

    create homepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from './components/pages/homepage/homepage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomepageComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
