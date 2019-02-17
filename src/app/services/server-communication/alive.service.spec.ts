8bd44316b0364288afebdb2505332ec13a0a918f
Gabriele Giunchi <gabriele.giunchi1994@gmail.com>
2019-02-17 12:00:40 +0100

    test

--- src/app/services/server-communication/alive.service.spec.ts
+++ src/app/services/server-communication/alive.service.spec.ts
@@ -1,9 +1,12 @@
 import {TestBed} from '@angular/core/testing';
 
 import {AliveService} from './alive.service';
+import {HttpClientModule} from '@angular/common/http';
 
 describe('AliveService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientModule]
+  }));
 
   it('should be created', () => {
     const service: AliveService = TestBed.get(AliveService);
