8c7429036897b7cb6af76842e063caed02f626a0
Gabriele Giunchi <gabriele.giunchi1994@gmail.com>
2019-05-01 14:08:14 +0200

    gym service

--- src/app/services/server-communication/gym.service.ts
+++ src/app/services/server-communication/gym.service.ts
@@ -1,6 +1,10 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {UrlService} from '../url.service';
+import {Observable} from 'rxjs';
+import {Gym} from 'src/app/model/entities/gym';
+import {CONSTANTS} from 'src/app/constants';
+import {City} from 'src/app/model/entities/city';
 
 @Injectable({
   providedIn: 'root'
@@ -9,5 +13,21 @@ export class GymService {
 
   constructor(private http: HttpClient, private urlService: UrlService) {}
 
+  get gyms(): Observable<Gym[]> {
+    return this.http.get<Gym[]>(this.urlService.getRestUrl(CONSTANTS.GYMS), this.urlService.authenticationHeader);
+  }
 
+  getGymById(id: number): Observable<Gym> {
+    return this.http.get<Gym>(this.urlService.getRestUrl(`${CONSTANTS.GYMS}/${id}`), this.urlService.authenticationHeader);
+  }
+
+  getGymsByCity(city: City): Observable<Gym[]> {
+    const url = this.urlService.getRestUrl(`${CONSTANTS.GYMS}/by_city/${city.id}`);
+    return this.http.get<Gym[]>(url, this.urlService.authenticationHeader);
+  }
+
+  getRatingOfGym(gym: Gym): Observable<number> {
+    const url = this.urlService.getRestUrl(`${CONSTANTS.GYMS}/${gym.id}/rating`);
+    return this.http.get<number>(url, this.urlService.authenticationHeader);
+  }
 }
