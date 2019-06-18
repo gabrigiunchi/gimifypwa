93faec762e45a5debe15000398eee866fe6a12fe
GABRIELE Giunchi <gabriele.giunchi@studio.unibo.it>
2019-06-18 14:00:26 +0200

    tests

--- src/app/components/modals/edit-avatar-bottom-sheet/edit-avatar-bottom-sheet.component.ts
+++ src/app/components/modals/edit-avatar-bottom-sheet/edit-avatar-bottom-sheet.component.ts
@@ -9,6 +9,13 @@ export enum EditAvatarAction {
   modifyCurrent,
 }
 
+interface Action {
+  title: string;
+  icon: string;
+  action: EditAvatarAction;
+  color?: string;
+}
+
 @Component({
   selector: 'app-edit-avatar-bottom-sheet',
   templateUrl: './edit-avatar-bottom-sheet.component.html',
@@ -16,27 +23,46 @@ export enum EditAvatarAction {
 })
 export class EditAvatarBottomSheetComponent {
 
+  actions: Action[] = [
+    {
+      action: EditAvatarAction.delete,
+      color: 'red',
+      icon: 'delete',
+      title: 'Restore default'
+    },
+
+    {
+      action: EditAvatarAction.modifyCurrent,
+      icon: 'crop_rotate',
+      title: 'Modify current avatar'
+    },
+
+    {
+      action: EditAvatarAction.chooseFromDefault,
+      icon: 'tag_faces',
+      title: 'Choose from default avatars'
+    },
+
+    {
+      action: EditAvatarAction.openGallery,
+      icon: 'insert_photo',
+      title: 'Choose from gallery'
+    },
+
+    {
+      action: EditAvatarAction.cancel,
+      icon: 'cancel',
+      title: 'Cancel'
+    },
+  ];
+
   constructor(private bottomSheetRef: MatBottomSheetRef<EditAvatarBottomSheetComponent>) {
   }
 
-  delete(): void {
-    this.bottomSheetRef.dismiss(EditAvatarAction.delete);
-  }
-
-  modify(): void {
-    this.bottomSheetRef.dismiss(EditAvatarAction.modifyCurrent);
-  }
-
-  chooseFromDefault(): void {
-    this.bottomSheetRef.dismiss(EditAvatarAction.chooseFromDefault);
-  }
-
-  cancel(): void {
-    this.bottomSheetRef.dismiss(EditAvatarAction.cancel);
-  }
-
-  openFilePicker(): void {
-    this.bottomSheetRef.dismiss(EditAvatarAction.openGallery);
-    document.getElementById('filePicker').click();
+  onSelection(action: Action): void {
+    this.bottomSheetRef.dismiss(action.action);
+    if (action.action === EditAvatarAction.openGallery) {
+      document.getElementById('filePicker').click();
+    }
   }
 }
